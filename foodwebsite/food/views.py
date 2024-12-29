49f6fc4ad11b240639385ef8f178ff2eb506f595
Sai-Harshith-15 <saiharshith15.git@gmail.com>
2024-12-29 10:35:34 +0530

    added the required files and started the working on django templates
    added the required files and started the working on django templates

---
+++ foodwebsite/food/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Item
+from django.template import loader
+
+# Create your views here.
+def index(request):
+    item_list = Item.objects.all()
+    template = loader.get_template('food/index.html')
+    context = {
+        'item_list': item_list,
+    }
+    return HttpResponse(template.render(context,request))
+
+def item(request):
+    return HttpResponse('<h1>This is an Item view</h1>')
+
