8b969afff73b804870cc37616b174b5ce4180697
Sai-Harshith-15 <saiharshith15.git@gmail.com>
2025-01-04 09:10:03 +0530

    updated code for role based user authentication and authorization
    updated code for role based user authentication and authorization

--- authentication_jwt/myproject/users/models.py
+++ authentication_jwt/myproject/users/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        PATIENT = "PATIENT", "Patient"
+        DOCTOR = "DOCTOR", "Doctor"
+
+    base_role = Role.ADMIN
+
+    role = models.CharField(max_length=50, choices=Role.choices)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.role = self.base_role
+            return super().save(*args, **kwargs)
+
+
+class PatientManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.PATIENT)
+
+
+class Patient(User):
+
+    base_role = User.Role.PATIENT
+
+    patient = PatientManager()
+
+    class Meta:
+        proxy = True
+
+    def welcome(self):
+        return "only for patients"
+
+# class PatientProfile(models.Model):
+#   user = models.OneToOneField(User, on_delete=models.CASCADE)
+#   patient_id = models.IntegerField(null=True, blank=True)
+
+
+class DoctorManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=User.Role.DOCTOR)
+
+class Doctor(User):
+
+    base_role = User.Role.DOCTOR
+
+    patient = DoctorManager()
+
+    class Meta:
+        proxy = True
+
+    def welcome(self):
+        return "only for doctors"
--- authentication_jwt/myproject/users/urls.py
+++ authentication_jwt/myproject/users/urls.py
@@ -1,9 +1,9 @@
-
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import UserAPI
+from .api import UserAPI
+
 urlpatterns = [
-  path('token/',TokenObtainPairView.as_view(), name='login'),
-  path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-  path('user/', UserAPI.as_view(), name='user'),
+    path("token/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("user/", UserAPI.as_view(), name="user"),
 ]
